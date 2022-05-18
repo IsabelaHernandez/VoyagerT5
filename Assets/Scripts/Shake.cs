3e185c449fd15b1628e3043c6739e0c30caffac6
sebastiancatano <sebastian.catano@upb.edu.co>
2022-05-17 19:24:34 -0500

    Movimiento Panel

--- Assets/Scripts/Shake.cs
+++ Assets/Scripts/Shake.cs
@@ -7,24 +7,32 @@ public class Shake : MonoBehaviour
     public MovPlaneta movPlaneta;
     public float duration = 1f;
     public bool start = false;
+    public GameObject panel;
+    public Transform target;
+    public Transform target2;
+    public float speed;
     void Start()
     {
-        
+        //target2.position = panel.transform.position;
     }
 
     void Update()
     {
+        float step = speed * Time.deltaTime;
         if (Input.GetKeyDown(KeyCode.Space) && movPlaneta.stop == 0)
         {
             start = true;
             StartCoroutine(Shaking());
+            while (Input.GetKeyDown(KeyCode.Space) && movPlaneta.stop == 0) { panel.transform.position = Vector3.MoveTowards(panel.transform.position, target.position, step); }
 
         }
         if (Input.GetKeyUp(KeyCode.Space))
         {
             start = false;
             StartCoroutine(Shaking());
+            while (Input.GetKeyUp(KeyCode.Space)) { panel.transform.position = Vector3.MoveTowards(panel.transform.position, target2.position, step); }
         }
+
         /*if (start)
         {
             start = false;
@@ -45,4 +53,14 @@ public class Shake : MonoBehaviour
         }
         transform.position = startPosition;
     }
+    /*void Accelerate()
+    {
+        float step = speed * Time.deltaTime;
+        if (start) { panel.transform.position = Vector3.MoveTowards(panel.transform.position, target.position, step); }
+    }
+    void Deaccelerate()
+    {
+        float step = speed * Time.deltaTime;
+        panel.transform.position = Vector3.MoveTowards(panel.transform.position, target2.position, step);
+    }*/
 }
